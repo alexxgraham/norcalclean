ec0f4df7b7ba176ed1fd51db5c0010bd5330bedf
alexxgraham <destgraham@icloud.com>
2025-02-02 20:03:43 -0800

    Update site 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://norcalproclean.com',
+  site: 'https://alexxgraham.github.io',
+  base: 'norcalclean',
   vite: {
     plugins: [tailwindcss()]
   }
