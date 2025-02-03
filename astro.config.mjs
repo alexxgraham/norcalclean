13ba43f165f2422a2fd8d69c516fb0114029dafd
alexxgraham <destgraham@icloud.com>
2025-02-02 21:57:40 -0800

    Update site 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -6,10 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://norcalproclean.com',
+
   devToolbar: {
     enabled: false
   },
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
